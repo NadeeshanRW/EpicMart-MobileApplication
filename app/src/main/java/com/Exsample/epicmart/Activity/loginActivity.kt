a1b092aa968f53a08ce7a2d589c6cdbad072448a
Nadeeshan R Wijesinghe <119121943+NadeeshanRW@users.noreply.github.com>
2024-07-28 19:38:29 +0530

    Data sent to the  firebase accounts Successfully. And after Registration correctly logging into the homepage.

--- app/src/main/java/com/Exsample/epicmart/Activity/loginActivity.kt
+++ app/src/main/java/com/Exsample/epicmart/Activity/loginActivity.kt
@@ -3,29 +3,90 @@ package com.Exsample.epicmart.Activity
 import android.content.Intent
 import android.os.Bundle
 import android.widget.TextView
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.Exsample.epicmart.R
+import com.Exsample.epicmart.databinding.ActivityLoginBinding
+import com.google.firebase.auth.FirebaseAuth
 
 class loginActivity : AppCompatActivity() {
+
+private  lateinit var binding: ActivityLoginBinding
+private lateinit var firebaseAuth: FirebaseAuth
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_login)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
 
-        val textView = findViewById<TextView>(R.id.Registerlink)
-        textView.setOnClickListener {
-            val intent = Intent(this, signupActivity::class.java)
+
+        binding = ActivityLoginBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        firebaseAuth = FirebaseAuth.getInstance()
+
+        binding.Registerlink.setOnClickListener{
+            val intent = Intent (this , signupActivity::class.java)
             startActivity(intent)
+        }
+
+        binding.button4.setOnClickListener{
+            val email = binding.editTextText.text.toString()
+            val pass = binding.editTextTextPassword.text.toString()
+
+
+
+            if (email.isNotEmpty() && pass.isNotEmpty() ) {
+
+
+                    firebaseAuth.signInWithEmailAndPassword(email , pass ).addOnCompleteListener {
+                        if (it.isSuccessful){
+                            val intent = Intent (this , HomeActivity ::class.java)
+                            startActivity(intent)
+                        }else{
+                            Toast.makeText(this, it.exception.toString(), Toast.LENGTH_SHORT).show()
+                        }
+                    }
+
+
+
+
+            } else {
+                Toast.makeText(this, "Empty Fields are not Allowed !", Toast.LENGTH_SHORT).show()
+            }
 
 
         }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+      //  ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main))
+
+      //  { v, insets ->
+       //     val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+     //       v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+       //     insets
+    //    }
+
+     //   val textView = findViewById<TextView>(R.id.Registerlink)
+      //  textView.setOnClickListener {
+    //        val intent = Intent(this, signupActivity::class.java)
+  //          startActivity(intent)
+
+
+   //     }
     }
 }
--- app/src/main/java/com/Exsample/epicmart/Activity/signupActivity.kt
+++ app/src/main/java/com/Exsample/epicmart/Activity/signupActivity.kt
@@ -4,36 +4,88 @@ import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
 import android.widget.TextView
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.Exsample.epicmart.R
+import com.Exsample.epicmart.databinding.ActivitySignupBinding
+import com.google.firebase.auth.FirebaseAuth
 
 class signupActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivitySignupBinding
+    private lateinit var firebaseAuth: FirebaseAuth
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_signup)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
 
-        val textView = findViewById<TextView>(R.id.loginpagelink)
-        textView.setOnClickListener {
-            val intent = Intent(this, loginActivity::class.java)
+        binding = ActivitySignupBinding.inflate(layoutInflater)
+
+        enableEdgeToEdge()
+        setContentView(binding.root)
+
+
+        firebaseAuth = FirebaseAuth.getInstance()
+
+        binding.loginpagelink.setOnClickListener{
+            val intent = Intent (this ,loginActivity ::class.java)
             startActivity(intent)
         }
 
-        var startbutton = findViewById<Button>(R.id.afterregistrationbutten)
-        startbutton.setOnClickListener {
-            val Intent = Intent(this, loginActivity::class.java)
-            startActivity(Intent)
+        binding.afterregistrationbutten.setOnClickListener {
+            val email = binding.editTextText13.text.toString()
+            val pass = binding.editTextText15.text.toString()
+            val cofirmpass = binding.editTextTextPassword.text.toString()
+
+
+            if (email.isNotEmpty() && pass.isNotEmpty() && cofirmpass.isNotEmpty()) {
+                if (pass == cofirmpass) {
+
+                    firebaseAuth.createUserWithEmailAndPassword(email , pass ).addOnCompleteListener {
+                        if (it.isSuccessful){
+                            val intent = Intent (this ,loginActivity ::class.java)
+                            startActivity(intent)
+                        }else{
+                            Toast.makeText(this, it.exception.toString(), Toast.LENGTH_SHORT).show()
+                        }
+                    }
+
+
+                } else {
+                    Toast.makeText(this, "Password is not matching", Toast.LENGTH_SHORT).show()
+                }
+
+            } else {
+                Toast.makeText(this, "Empty Fields are not Allowed !", Toast.LENGTH_SHORT).show()
+            }
+
+
+            //   ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            //       val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            //       v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            //       insets
+
+
+            //   }
+
+
+            //   val textView = findViewById<TextView>(R.id.loginpagelink)
+            //     textView.setOnClickListener {
+            //       val intent = Intent(this, loginActivity::class.java)
+            //        startActivity(intent)
+            //     }
+
+            //     var startbutton = findViewById<Button>(R.id.afterregistrationbutten)
+            //   startbutton.setOnClickListener {
+            //       val Intent = Intent(this, loginActivity::class.java)
+            //        startActivity(Intent)
+            //   }
+
+
         }
-
-
-
     }
 }
