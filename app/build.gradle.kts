44b832788302d190cc9fe7da13c7cea0f29fbef8
Nadeeshan R Wijesinghe <119121943+NadeeshanRW@users.noreply.github.com>
2024-07-30 01:32:26 +0530

    crate home page interface and best seller.xml and catogory.cml page

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -49,6 +49,22 @@ dependencies {
     implementation(libs.firebase.database)
     implementation(libs.firebase.auth)
     testImplementation(libs.junit)
+
+
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
+
+
+
+    implementation ("androidx.lifecycle:lifecycle-extensions:2.2.0")
+    implementation ("androidx.lifecycle:lifecycle-runtime-ktx:2.6.1")
+    implementation ("androidx.lifecycle:lifecycle-viewmodel-ktx:2.6.1")
+    implementation ("androidx.lifecycle:lifecycle-livedata-ktx:2.6.1")
+
+
+    implementation ("androidx.activity:activity-ktx:1.4.0")
+
+    implementation ("com.github.bumptech.glide:glide:4.12.0")
+    implementation ("com.google.code.gson:gson:2.9.1")
+    implementation ("com.tbuonomo:dotsindicator:5.0")
 }
